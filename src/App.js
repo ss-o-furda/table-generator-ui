86ce1fe79853728598f2794096ca1976dcd98743
ss-o-furda <orik7800@gmail.com>
2022-01-20 20:15:18 +0200

    Create common routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import styles from "./App.module.scss";
+import routes from "./routes";
+const Header = lazy(() => import("./components/Header"));
 
-function App() {
+const App = () => {
   return (
-    <div className={styles.App}>
-      <h1>Hello world!</h1>
-    </div>
+    <Suspense fallback={null}>
+      <div className={styles.App}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path={routes.generate} component={null} />
+            <Route path={routes.pricing} component={null} />
+            <Route path={routes.about} component={null} />
+            <Route path={routes.profile} component={null} />
+            <Route path={routes.dashboard} component={null} />
+            <Route path={routes.main} component={null} />
+            <Route path="*" element={<Navigate to={routes.main} />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Suspense>
   );
-}
+};
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+export default Object.freeze({
+  main: "/",
+  login: "/login",
+  about: "/about",
+  generate: "/generate",
+  pricing: "/pricing",
+  profile: "/profile",
+  dashboard: "/dashboard",
+});
